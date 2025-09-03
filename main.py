b7b512566d303c400cbbe9790abd5ea136556e1e
novahMax2552 <hei.vahatra@gmail.com>
2025-09-03 11:47:55 +0300

    Characteristic

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from typing import List
 
 app = FastAPI()
+
+cars_db = []
+
+class Characteristic(BaseModel):
+    max_speed: float = Field(..., gt=0, description="Vitesse maximale en km/h")
+    max_fuel_capacity: float = Field(..., gt=0, description="Capacité maximale du réservoir en litres")
 
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
+
+
+
+
+    
